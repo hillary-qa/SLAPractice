1b50ab3d51c8eac5baa26cfbba587f5d3d6bcd41
hillary-qa <hillquinnaustin@gmail.com>
2020-06-18 16:07:16 -0400

    Script to do a one-way ANOVA

---
+++ Scripts/SLA_ANOVA.R
@@ -0,0 +1,28 @@
+SLA<-read.csv("Data/SLA_Canadian cities.csv")
+
+library(vegan)
+library(car)
+library(tidyverse)
+
+glimpse(SLA)
+
+unique(SLA$City)
+
+## Histogram
+
+histogram <- ggplot(SLA, aes(x=value, fill=City))+
+  facet_wrap(~City)+
+  geom_histogram(bins=50)
+histogram
+
+## one way ANOVA
+
+ANOVA<- lm(value ~ City, data=SLA)
+summary(ANOVA)
+Anova(ANOVA, type = "2")
+#Response: value
+#Sum Sq   Df F value Pr(>F)
+#City         889    5  1.5407 0.1739
+#Residuals 253337 2195 
+
+
